5db18e6fce7743da7de026611caa9f174a1246d5
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 09:00:32 +0530

    definition of addRecursive method

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -15,8 +15,23 @@ public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
     public int numberOfLeftChild = 0;
     private int maxLevel = 0;
 
+    public TreeNode<E> getRoot() {
+        return root;
+    }
+
     @Override
     public boolean data(E data) {
-        return false;
+        root = addRecursive(root,data);
+        return true;
+    }
+    private TreeNode<E> addRecursive(TreeNode<E> currentNode, E data) {
+        if (currentNode == null) {
+            return new TreeNode<>(data);
+        } else if ((Integer) data < (Integer) currentNode.getData()) {
+            currentNode.setLeftChild(addRecursive(currentNode.getLeftChild(), data));
+        } else if ((Integer) data > (Integer) currentNode.getData()) {
+            currentNode.setRightChild(addRecursive(currentNode.getRightChild(), data));
+        }
+        return currentNode;
     }
 }
