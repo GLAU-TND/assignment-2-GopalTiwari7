d50eb74208fccccdce1a29f18dcf8ad56b632833
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 11:39:11 +0530

    define variables and make constructor,getter and setters and toString method

--- src/problem5/student/Student.java
+++ src/problem5/student/Student.java
@@ -7,4 +7,39 @@
 package problem5.student;
 // to store student information and properties
 public class Student {
+    private int rollNumber;
+    private String name;
+
+    public Student(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public Student(int rollNumber, String name) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNumber=" + rollNumber +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
