49815f89451fce3bbe2bff0789c10cfa8da6adca
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 11:36:30 +0530

    implements CircularQueueADT interface and made methods in it

--- src/problem5/circularqueue/MyCircularQueue.java
+++ src/problem5/circularqueue/MyCircularQueue.java
@@ -1,11 +1,35 @@
-/*
- *  Created by IntelliJ IDEA.
- *  User: Vaibhav
- *  Date: 23-Mar-20
- *  Time: 7:06 PM
- */
 package problem5.circularqueue;
-//to implement circular queue
-public class MyCircularQueue {
 
+import problem3.node.Node;
+import problem5.adt.CircularQueueADT;
+
+//to implement circular queue
+public class MyCircularQueue<E> implements CircularQueueADT<E> {
+    private Node<E> rear;
+    private int size;
+
+    @Override
+    public void enqueue(E data) {
+
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
 }
