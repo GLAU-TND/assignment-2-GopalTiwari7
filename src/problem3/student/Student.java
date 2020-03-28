a129455478271d7980a9f5a8c9cee8d402ac07bd
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 10:11:32 +0530

    made constructors , getters and setters

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -3,4 +3,28 @@ package problem3.student;
 public class Student {
     private int rollNumber;
     private String name;
+
+    public Student(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
