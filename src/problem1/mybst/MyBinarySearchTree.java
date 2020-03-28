921bbe1d25b0ea74840e8d10eab8bc3004c7be38
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 08:55:38 +0530

    initialise variables

--- src/problem1/mybst/MyBinarySearchTree.java
+++ src/problem1/mybst/MyBinarySearchTree.java
@@ -5,8 +5,15 @@
  *  Time: 7:17 PM
  */
 package problem1.mybst;
+
+import problem1.node.TreeNode;
+
 // to implement BinarySearchTree
 public class MyBinarySearchTree<E> implements MyBinarySearchTreeADT<E> {
+
+    private TreeNode<E> root;
+    public int numberOfLeftChild = 0;
+    private int maxLevel = 0;
 
     @Override
     public boolean data(E data) {
