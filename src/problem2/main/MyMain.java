4ec368a87389b21a861b7145fe23f7f1e0ffeb8c
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 09:49:33 +0530

    initialise binary tree and add elements in it and and craete traversePreOrder and traversePostOrder methods

--- src/problem2/main/MyMain.java
+++ src/problem2/main/MyMain.java
@@ -5,7 +5,38 @@
  *  Time: 7:15 PM
  */
 package problem2.main;
+
+import problem1.mybst.MyBinarySearchTree;
+import problem1.node.TreeNode;
+
 // executable class
 // use problem1.mybst.MyBinarySearchTree class for creating binary tree
 public class MyMain {
+    public static void main(String[] args) {
+        MyBinarySearchTree<Integer> myBinarySearchTree = new MyBinarySearchTree<>();
+        myBinarySearchTree.add(12);
+        myBinarySearchTree.add(45);
+        myBinarySearchTree.add(76);
+        myBinarySearchTree.add(32);
+        myBinarySearchTree.add(20);
+        System.out.println("Traverse the tree in preOrder:");
+        traversePreOrder(myBinarySearchTree.getRoot());
+        System.out.println();
+        System.out.println("Traverse the tree in postOrder:");
+        traversePostOrder(myBinarySearchTree.getRoot());
+    }public static void traversePreOrder(TreeNode<Integer> currentNode) {
+        if (currentNode != null) {
+            System.out.print(currentNode.getData() + " ");
+            traversePreOrder(currentNode.getLeftChild());
+            traversePreOrder(currentNode.getRightChild());
+        }
+    }
+
+    public static void traversePostOrder(TreeNode<Integer> currentNode) {
+        if (currentNode != null) {
+            traversePostOrder(currentNode.getLeftChild());
+            traversePostOrder(currentNode.getRightChild());
+            System.out.print(currentNode.getData() + " ");
+        }
+    }
 }
