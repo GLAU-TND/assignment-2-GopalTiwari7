7f360ccf37f8d281fa1fe06e29115f4e4b4e8af3
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 15:42:17 +0530

    enter number of element in a queue

--- src/problem5/main/MyMain.java
+++ src/problem5/main/MyMain.java
@@ -9,10 +9,16 @@ package problem5.main;
 import problem5.circularqueue.MyCircularQueue;
 import problem5.student.Student;
 
+import java.util.Scanner;
+
 //executable class
 public class MyMain {
     public static void main(String[] args) {
         MyCircularQueue<Student> myCircularQueue = new MyCircularQueue<>();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number of Students you want to add in queue :");
+        int n = scanner.nextInt();
+        scanner.nextLine();
     }
 
 }
