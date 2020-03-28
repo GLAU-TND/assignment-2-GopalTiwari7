2d0143ac7dcb98d78bec01b42b4be8d2a16a7f1b
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2020-03-28 16:10:27 +0530

    create getters and setters of data and next

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -52,5 +52,21 @@ public class MyQueue<E> implements MyQueueADT<E> {
             this.data = data;
             this.next = next;
         }
+
+        public E getData() {
+            return data;
+        }
+
+        public Node<E> getNext() {
+            return next;
+        }
+
+        public void setData(E data) {
+            this.data = data;
+        }
+
+        public void setNext(Node<E> next) {
+            this.next = next;
+        }
     }
 }
